00fcbc68ddbcfda5661fb0576aa765921d804a12
tendi <pastendisherpa26@gmail.com>
2023-12-22 19:04:58 +0545

    created required pages and homepage content

---
+++ app/(dashboard)/new-tour/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewTourPage = () => {
+  return <div>NewTourPage</div>
+}
+
+export default NewTourPage
--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1 className='text-4xl'>Hello world</h1>
+import Link from 'next/link'
+
+export default function HomePage() {
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-6xl font-bold text-primary'>My assistant</h1>
+          <p className='py-6 text-lg leading-loose'>
+            This is developed to play a role of assitant for me but it can be
+            your assitant too. AI is the future and this app can be your AI
+            companion to enhance your conversations, content creation and more.
+          </p>
+          <Link href='/chat' className='btn btn-secondary'>
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
