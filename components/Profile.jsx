0934624fca7ce834f8b23f993043e66efa56b0ac
tendi <pastendisherpa26@gmail.com>
2023-12-23 21:49:26 +0545

    setting user button with signout option by using clerk

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { UserButton, auth, currentUser } from '@clerk/nextjs'
 
-const Profile = () => {
-  return <div>UserProfile</div>
+const Profile = async () => {
+  const user = await currentUser()
+  // const { userId } = auth()
+  return (
+    <div className='px-4 flex items-center gap-2'>
+      <UserButton afterSignOutUrl='/' />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  )
 }
 
 export default Profile
