b997e98eb315bb0b79994f3c5c5f03e4ff03ee10
tendi <pastendisherpa26@gmail.com>
2023-12-23 22:12:54 +0545

    setting theme using daisyui

--- components/ThemeBtn.jsx
+++ components/ThemeBtn.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 import { BsMoonFill, BsSunFill } from 'react-icons/bs'
 
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+}
+
 const ThemeBtn = () => {
+  const [theme, setTheme] = useState(themes.dracula)
+  const toggleTheme = () => {
+    const newTheme = theme === themes.winter ? themes.dracula : themes.winter
+    document.documentElement.setAttribute('data-theme', newTheme)
+    setTheme(newTheme)
+  }
   return (
-    <button className='btn btn-primary btn-sm'>
-      <BsMoonFill size={16} />
+    <button className='btn btn-outline  btn-sm' onClick={toggleTheme}>
+      {theme === 'winter' ? <BsMoonFill size={16} /> : <BsSunFill size={16} />}
     </button>
   )
 }
