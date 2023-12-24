8ee827be30d0b38798f806c2300d25c7141a8b94
tendi <pastendisherpa26@gmail.com>
2023-12-24 10:58:16 +0545

    integration of open ai and completion of chat app

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,22 @@
 'use server'
+import OpenAI from 'openai'
 
-export const chatResponse = async (message) => {
-  console.log(message)
-  return 'good'
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+export const AIResponse = async (messages) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'you are a helpful assitant' },
+        ...messages,
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+    })
+    return response.choices[0].message
+  } catch (error) {
+    return null
+  }
 }
